e11f960a07c86c1e40fe9d19e427d31131dc2638
Юрий Данилов <yuradan0903@gmail.com>
2024-01-06 00:43:49 +0500

    Initial commit

---
+++ ClinicSiteBack/clinicsite/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.login_view, name='login')]
