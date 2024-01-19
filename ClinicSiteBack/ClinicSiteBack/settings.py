a0bc5f8c4086de43d3eddbbfad53b7430b8d60b6
Юрий Данилов <yuradan0903@gmail.com>
2024-01-19 20:32:54 +0300

    ~market card slider update

--- ClinicSiteBack/ClinicSiteBack/settings.py
+++ ClinicSiteBack/ClinicSiteBack/settings.py
@@ -134,3 +134,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DATA_UPLOAD_MAX_MEMORY_SIZE = 20480000
