c40b95b4c8d8e6a13a7ec5185cb55af2574d69b0
Юрий Данилов <yuradan0903@gmail.com>
2024-01-22 20:45:42 +0300

    ~sorting update

--- ClinicSiteBack/clinicsite/views.py
+++ ClinicSiteBack/clinicsite/views.py
@@ -1,4 +1,6 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.template.loader import render_to_string
 
 from .models import Product
 
@@ -9,16 +11,45 @@ def about_view(request):
     return render(request, 'about.html')
 
 def market_view(request):
-    # Получение продуктов из базы данных
+    search_query = request.GET.get('search', '')
+    sort_order = request.GET.get('sort', 'asc')  # По умолчанию сортируем по возрастанию
+
+    # Получаем все товары и фильтруем по поисковому запросу
     products = Product.objects.all()
+    if search_query:
+        products = products.filter(name__icontains=search_query)
 
-    # Разделение продуктов на два списка для двух разных card-container
-    first_three_products = products[:3]
-    next_eight_products = products[3:11]
+    # Сортировка товаров
+    if sort_order == 'desc':
+        products = products.order_by('-price')
+    else:
+        products = products.order_by('price')
 
-    # Передача продуктов в шаблон
+    # Фильтрация по типам
+    product_types = request.GET.getlist('type')
+    if product_types:
+        products = products.filter(type__in=product_types)
+
+    # Фильтрация по странам
+    countries = request.GET.getlist('country')
+    if countries:
+        products = products.filter(country_of_origin__in=countries)
+
+    # Фильтрация по цене
+    min_price = request.GET.get('min_price')
+    max_price = request.GET.get('max_price')
+    if min_price and max_price:
+        products = products.filter(price__gte=min_price, price__lte=max_price)
+
+    # Если это AJAX-запрос, возвращаем только карточки товаров
+    if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+        html = render_to_string('product_cards.html', {'products': products}, request=request)
+        return JsonResponse({'html': html})
+
+    # Если это не AJAX-запрос, то возвращаем все товары
     context = {
-        'first_three_products': first_three_products,
-        'next_eight_products': next_eight_products
+        'products': products
     }
     return render(request, 'market.html', context)
+
+
